3a020d98ccc97c71dda8fc65f6ca0c7367f71693
vfseanm <vfseanm@gmail.com>
2014-03-23 20:23:33 -0400

    deleting images on posts!

--- post.php
+++ post.php
@@ -112,36 +112,7 @@ include('navbar.php');
 
             <div class="col-lg-4">
                 
-                <!-- /well -->
-                <div class="well">
-                    <h4>Popular Blog Categories</h4>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#dinosaurs">Dinosaurs</a>
-                                </li>
-                                <li><a href="#spaceships">Spaceships</a>
-                                </li>
-                                <li><a href="#fried-foods">Fried Foods</a>
-                                </li>
-                                <li><a href="#wild-animals">Wild Animals</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#alien-abductions">Alien Abductions</a>
-                                </li>
-                                <li><a href="#business-casual">Business Casual</a>
-                                </li>
-                                <li><a href="#robots">Robots</a>
-                                </li>
-                                <li><a href="#fireworks">Fireworks</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                
                 <!-- /well -->
                 <div class="well">
                     <h4>Upcoming Events</h4>
@@ -187,7 +158,10 @@ include('navbar.php');
              <div class="form-group">
                 <label for="curfile" class="col-sm-2 control-label">Current Image</label>
                 <div class="col-sm-10" id="curfile">
-                    <?php echo $image ?>
+                    <?php if (strcmp($image, "")!=0){
+                    echo $image; ?>
+                    <a style="padding-left:15px" class="red" href="/post_delete_image.php?ID=<?php echo $postid ?>"><i class="fa fa-times"></i>Delete Image</a>
+                <?php } ?>
                 </div>
             </div>
             <div class="form-group">
